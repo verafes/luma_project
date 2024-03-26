0913253929fffb5a0cac19e6f315946375f412b1
Vera Fesianava <verafes@gmail.com>
2024-03-25 19:19:48 -0700

    RF: mark flaky tests (#8)

--- tests/test_reviews_page.py
+++ tests/test_reviews_page.py
@@ -1,3 +1,5 @@
+import pytest
+
 from selenium.webdriver.support import expected_conditions as EC
 from locators.reviews_page_locators import ReviewsPageLocators
 from selenium.webdriver.support.wait import WebDriverWait
@@ -60,6 +62,7 @@ class TestReviews:
             print('Leave a review failed')
             assert review_successfully_submitted == "You submitted your review for moderation." or 'https://magento.softwaretestingboard.com/review/product/post/id' in result_CI_GitHub_Action_URL, "Leave a review failed"
 
+    @pytest.mark.xfail(reason="Flaky behavior on CI")
     def test_tc_01_15_02_check_that_it_is_possible_to_vote_for_2_star(self, driver):
         """
         The user is NOT logged in, located in the product card, section "Reviews"
@@ -115,6 +118,7 @@ class TestReviews:
             print('Leave a review failed')
             assert review_successfully_submitted == "You submitted your review for moderation." or 'https://magento.softwaretestingboard.com/review/product/post/id' in result_CI_GitHub_Action_URL, "Leave a review failed"
 
+    @pytest.mark.xfail(reason="Flaky behavior on CI")
     def test_tc_01_15_03_check_that_it_is_possible_to_vote_for_3_star(self, driver):
         """
         The user is NOT logged in, located in the product card, section "Reviews"
@@ -166,6 +170,7 @@ class TestReviews:
             print('Leave a review failed')
             assert review_successfully_submitted == "You submitted your review for moderation." or 'https://magento.softwaretestingboard.com/review/product/post/id' in result_CI_GitHub_Action_URL, "Leave a review failed"
 
+    @pytest.mark.xfail(reason="Flaky behavior on CI")
     def test_tc_01_15_04_check_that_it_is_possible_to_vote_for_4_star(self, driver):
         """
         The user is NOT logged in, located in the product card, section "Reviews"
@@ -217,6 +222,7 @@ class TestReviews:
             print('Leave a review failed')
             assert review_successfully_submitted == "You submitted your review for moderation." or 'https://magento.softwaretestingboard.com/review/product/post/id' in result_CI_GitHub_Action_URL, "Leave a review failed"
 
+    @pytest.mark.xfail(reason="Flaky behavior on CI")
     def test_tc_01_15_05_check_that_it_is_possible_to_vote_for_5_star(self, driver):
         """
         The user is NOT logged in, located in the product card, section "Reviews"
